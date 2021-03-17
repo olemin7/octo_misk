51350a1b9a820ac97ab113ab26c52857b31ccdef
Oleksandr Minenko <ominenko@luxoft.com>
2021-03-17 22:05:05 +0200

    fix

--- octo_misk/static/js/misk.js
+++ octo_misk/static/js/misk.js
@@ -6,8 +6,11 @@
  */
 $(function() {
     function miskViewModel(parameters) {
-      console.log("miskViewModel:");
-      self.isOperational = ko.observable(true);
+      var self = this;
+      self.isOperational = ko.observable();
+      self.isOperational.subscribe(function(newValue) {
+        $('#navbarLedDropdown').prop('disabled', !newValue);
+        });
       self.fromCurrentData = function (data) {
           self._processStateData(data.state);
       };
@@ -17,7 +20,7 @@ $(function() {
       };
 
       self._processStateData = function (data) {
-        console.log("miskViewModel:"+data.flags);
+        //console.log("miskViewModel:",data.flags);
         self.isOperational(data.flags.operational);
       };
     }
@@ -25,6 +28,7 @@ $(function() {
     OCTOPRINT_VIEWMODELS.push({
         construct: miskViewModel,
         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies:["printerStateViewModel"],
         elements: ["#navbar_plugin_misk"]
     });
 });
